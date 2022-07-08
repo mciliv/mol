3e4185a443938f1494605174bfe8ccbbca4c81a1
M <refits.minxes0h@icloud.com>
2022-07-08 15:44:10 -0700

    Adding dataframe

--- sheets_get_values.py
+++ sheets_get_values.py
@@ -2,8 +2,7 @@ from __future__ import print_function
 
 import os
 import sys
-import pdb
-import argparse
+import pdb import argparse
 
 import google.auth
 from google.auth.transport.requests import Request
@@ -12,7 +11,11 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import numpy as np
+import pandas as pd
 from sklearn import cluster 
+import libscol
+
+import config
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
@@ -40,7 +43,7 @@ def get_creds():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                config.CREDENTIALS, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
@@ -80,12 +83,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Pass: spreadsheet_id, and range_name
-    tables = get_values(SPREADSHEET_ID, [GLUCOSE_RANGE, NANOTEMPER_RANGE])
-    if 'range' in tables.keys():
-#        X = np.array(tables['range'])
-        X = pd.DataFrame(tables['range'])
-        print(X)
-        exit()
+    tables = get_values(SPREADSHEET_ID, 'Sheet1') 
+    if 'values' in tables.keys():
+        X = np.array(tables['values'])
     elif 'valueRanges' in tables.keys():
         valueRanges = tables['valueRanges']
         arrays = []
@@ -93,13 +93,15 @@ if __name__ == '__main__':
             values = valueRange['values']
             array = np.array(values)
             arrays.append(array)
-        X = np.concatenate(arrays, axis=1) 
+        X = np.concatenate(arrays, axis=1)
     else:
         raise Error("The key 'range' or 'valueRanges' isn't found")
-
     X[X == '#N/A (No matches are found in FILTER evaluation.)'] = np.nan
-    X = X.astype('float')
-    print("Shape without Nan removal ", X.shape)
+    X = pd.DataFrame(X)
+    X = X.iloc[176:276, [libscol.scol2int(GLUCOSE_10_COLUMN)] + list(range(libscol.scol2int(NANOTEMPER_START_COLUMN), libscol.scol2int(NANOTEMPER_END_COLUMN) + 1)))]
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print(X)
+    print("Shape without NaN removal ", X.shape)
     X = X[~np.isnan(X).any(axis=1)]
     print("Shape after NaN removal ", X.shape)
     kmeans = cluster.KMeans(n_clusters=args.K, random_state=0).fit(X)
