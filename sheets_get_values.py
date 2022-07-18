8f6d61ab484a14badcb1e1f5cd6fc355a47287d4
M <refits.minxes0h@icloud.com>
2022-07-18 11:18:18 -0700

    Added kmeans to dataframe

--- sheets_get_values.py
+++ sheets_get_values.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 
 import os
 import sys
-import pdb import argparse
+import pdb
+import argparse
 
 import google.auth
 from google.auth.transport.requests import Request
@@ -61,14 +62,14 @@ def get_values(spreadsheet_id, range_names):
     # pylint: disable=maybe-no-member
     try:
         service = build('sheets', 'v4', credentials=get_creds())
+        restResource = service.spreadsheets().values()
         if type(range_names) is str:
             result = service.spreadsheets().values().get(
-                spreadsheetId=spreadsheet_id, range=range_names, valueRenderOption='UNFORMATTED_VALUE').execute()
+                spreadsheetId=spreadsheet_id, range=range_names, majorDimension='COLUMNS', valueRenderOption='UNFORMATTED_VALUE').execute()
             rows = result.get('values', [])
             print(f"{len(rows)} rows retrieved")
-        elif type(range_names) is list:
-            result = service.spreadsheets().values().batchGet(
-                spreadsheetId=spreadsheet_id, ranges=range_names, valueRenderOption='UNFORMATTED_VALUE').execute()
+        elif type(range_names) is list: result = service.spreadsheets().values().batchGet(
+                spreadsheetId=spreadsheet_id, ranges=range_names, majorDimension='COLUMNS', valueRenderOption='UNFORMATTED_VALUE').execute()
         else:
             raise Error("'range_names' should be of type str or list")
         return result
@@ -76,35 +77,43 @@ def get_values(spreadsheet_id, range_names):
         print(f"An error occurred: {error}")
         return error
 
+def set_pandas_to_max_view():
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)  # more options can be specified also
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run K-means.")
     parser.add_argument('-K', type=int)
     args = parser.parse_args()
-
+    set_pandas_to_max_view()
+    
     # Pass: spreadsheet_id, and range_name
-    tables = get_values(SPREADSHEET_ID, 'Sheet1') 
+    tables = get_values(SPREADSHEET_ID, 'Sheet1' + '!A1:AC651') 
     if 'values' in tables.keys():
-        X = np.array(tables['values'])
+        spreadsheet = pd.concat(map(pd.Series, tables['values']), axis=1)
     elif 'valueRanges' in tables.keys():
         valueRanges = tables['valueRanges']
-        arrays = []
+        ranges = []
         for valueRange in valueRanges:
             values = valueRange['values']
-            array = np.array(values)
-            arrays.append(array)
-        X = np.concatenate(arrays, axis=1)
+            range = pd.concat(map(pd.Series, values), axis=1)
+            ranges.append(range)
+        spreadsheet = pd.concatenate(map(pd.Series, ranges), axis=1)
     else:
         raise Error("The key 'range' or 'valueRanges' isn't found")
-    X[X == '#N/A (No matches are found in FILTER evaluation.)'] = np.nan
-    X = pd.DataFrame(X)
-    X = X.iloc[176:276, [libscol.scol2int(GLUCOSE_10_COLUMN)] + list(range(libscol.scol2int(NANOTEMPER_START_COLUMN), libscol.scol2int(NANOTEMPER_END_COLUMN) + 1)))]
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-        print(X)
-    print("Shape without NaN removal ", X.shape)
-    X = X[~np.isnan(X).any(axis=1)]
+    spreadsheet[spreadsheet == '#N/A (No matches are found in FILTER evaluation.)'] = np.nan
+    pdb.set_trace()
+    rows = slice(COMPOUND_START_COLUMN, COMPOUND_END_COLUMN + 1)
+    cols = [libscol.scol2int(GLUCOSE_10_COLUMN)] + [*range(libscol.scol2int(NANOTEMPER_START_COLUMN), libscol.scol2int(NANOTEMPER_END_COLUMN) + 1)]
+    relevant = spreadsheet.iloc[rows, cols]
+    print("Shape without NaN removal ", relevant.shape)
+    X = relevant.dropna()
     print("Shape after NaN removal ", X.shape)
-    kmeans = cluster.KMeans(n_clusters=args.K, random_state=0).fit(X)
-    print("kmeans.labels_  \n", kmeans.labels_)
-    print("kmeans.cluster_centers_  \n", kmeans.cluster_centers_)
-
+    kmeans = cluster.KMeans(random_state=0) if args.K == None else cluster.KMeans(n_clusters=args.K, random_state=0)
+    kmeans_result = kmeans.fit(X)
+    centers = pd.DataFrame(kmeans.cluster_centers_.round(2))
+    print("kmeans centers \nEach row is a center\nThe indices on the left are the labels\n", centers)
+    results = pd.concat([relevant, pd.Series(kmeans.labels_, index=X.index, dtype='Int64', name='kmeans cluster')], axis=1)
+    print("\nRelevant data from spreadsheet with its determined cluster\n", results)
