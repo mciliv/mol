6f072f3828393a128b62ec4ba034fb31d1153325
M <refits.minxes0h@icloud.com>
2022-07-20 16:18:05 -0700

    In the middle of adding insertion of results to spreadsheet

--- kmeans.py
+++ kmeans.py
@@ -23,13 +23,13 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 SPREADSHEET_ID = '1VRl8dghA5t1JiLEa47OWW-hr-Qbvn9yksB4Hrga19Ck'
 GLUCOSE_10_COLUMN = "D"
-COMPOUND_START_COLUMN = 176
-COMPOUND_END_COLUMN = 276
+COMPOUND_START_ROW = 176
+COMPOUND_END_ROW = 276
 NANOTEMPER_START_COLUMN = "J"
 NANOTEMPER_END_COLUMN = "M"
 
-GLUCOSE_RANGE = GLUCOSE_10_COLUMN + str(COMPOUND_START_COLUMN) + ":" + GLUCOSE_10_COLUMN + str(COMPOUND_END_COLUMN)
-NANOTEMPER_RANGE = NANOTEMPER_START_COLUMN + str(COMPOUND_START_COLUMN) + ":" + NANOTEMPER_END_COLUMN + str(COMPOUND_END_COLUMN)
+GLUCOSE_RANGE = GLUCOSE_10_COLUMN + str(COMPOUND_START_ROW) + ":" + GLUCOSE_10_COLUMN + str(COMPOUND_END_ROW)
+NANOTEMPER_RANGE = NANOTEMPER_START_COLUMN + str(COMPOUND_START_ROW) + ":" + NANOTEMPER_END_COLUMN + str(COMPOUND_END_ROW)
 
 def get_creds():
     creds = None
@@ -66,12 +66,56 @@ def get_values(spreadsheet_id, range_names):
         if type(range_names) is str:
             result = service.spreadsheets().values().get(
                 spreadsheetId=spreadsheet_id, range=range_names, majorDimension='COLUMNS', valueRenderOption='UNFORMATTED_VALUE').execute()
-            rows = result.get('values', [])
-            print(f"{len(rows)} rows retrieved")
         elif type(range_names) is list: result = service.spreadsheets().values().batchGet(
                 spreadsheetId=spreadsheet_id, ranges=range_names, majorDimension='COLUMNS', valueRenderOption='UNFORMATTED_VALUE').execute()
         else:
             raise Error("'range_names' should be of type str or list")
+        return result
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+def update_values(spreadsheet_id, range_names, value_input_option,
+                  values):
+    """
+    Creates the batch_update the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.\n"
+        """
+    # pylint: disable=maybe-no-member
+    try:
+        service = build('sheets', 'v4', credentials=get_creds())
+        body = {
+            'values': values
+        }
+        result = service.spreadsheets().values().update(
+            spreadsheetId=spreadsheet_id, range=range_name,
+            valueInputOption=value_input_option, body=body).execute()
+        print(f"{(result.get('updates').get('updatedCells'))} cells updated.")
+        return result
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+def batch_update_values(spreadsheet_id, range_names,
+                        value_input_option, data):
+    """
+        Creates the batch_update the user has access to.
+        Load pre-authorized user credentials from the environment.
+        TODO(developer) - See https://developers.google.com/identity
+        for guides on implementing OAuth2 for the application.\n"
+            """
+    # pylint: disable=maybe-no-member
+    try:
+        service = build('sheets', 'v4', credentials=get_creds())
+        body = {
+            'valueInputOption': value_input_option,
+            'data': data
+        }
+        result = service.spreadsheets().values().batchUpdate(
+            spreadsheetId=spreadsheet_id, body=body).execute()
+        print(f"{(result.get('totalUpdatedCells'))} cells updated.")
         return result
     except HttpError as error:
         print(f"An error occurred: {error}")
@@ -82,15 +126,11 @@ def set_pandas_to_max_view():
     pd.set_option('display.max_columns', None)
     pd.set_option('display.width', None)  # more options can be specified also
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Run K-means.")
-    parser.add_argument('-K', type=int)
-    args = parser.parse_args()
+def spreadsheet_to_dataframe():
     set_pandas_to_max_view()
     
     # Pass: spreadsheet_id, and range_name
-    tables = get_values(SPREADSHEET_ID, 'Sheet1' + '!A1:AC651') 
+    tables = get_values(SPREADSHEET_ID, 'Sheet1') 
     if 'values' in tables.keys():
         spreadsheet = pd.concat(map(pd.Series, tables['values']), axis=1)
     elif 'valueRanges' in tables.keys():
@@ -104,16 +144,35 @@ if __name__ == '__main__':
     else:
         raise Error("The key 'range' or 'valueRanges' isn't found")
     spreadsheet[spreadsheet == '#N/A (No matches are found in FILTER evaluation.)'] = np.nan
-    pdb.set_trace()
-    rows = slice(COMPOUND_START_COLUMN, COMPOUND_END_COLUMN + 1)
-    cols = [libscol.scol2int(GLUCOSE_10_COLUMN)] + [*range(libscol.scol2int(NANOTEMPER_START_COLUMN), libscol.scol2int(NANOTEMPER_END_COLUMN) + 1)]
-    relevant = spreadsheet.iloc[rows, cols]
+    return spreadsheet
+
+def display_terminal(relevant, X, centers, results):
     print("Shape without NaN removal ", relevant.shape)
-    X = relevant.dropna()
     print("Shape after NaN removal ", X.shape)
+    print("\n", centers)
+    print("\n", results)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run K-means.")
+    parser.add_argument('-K', type=int)
+    args = parser.parse_args()
+    rows = slice(COMPOUND_START_ROW - 1, COMPOUND_END_ROW)
+    cols = [libscol.scol2int(GLUCOSE_10_COLUMN)] + [*range(libscol.scol2int(NANOTEMPER_START_COLUMN), libscol.scol2int(NANOTEMPER_END_COLUMN) + 1)]
+    spreadsheet = spreadsheet_to_dataframe()
+    relevant = spreadsheet.iloc[rows, cols]
+    X = relevant.dropna()
     kmeans = cluster.KMeans(random_state=0) if args.K == None else cluster.KMeans(n_clusters=args.K, random_state=0)
     kmeans_result = kmeans.fit(X)
     centers = pd.DataFrame(kmeans.cluster_centers_.round(2))
-    print("kmeans centers \nEach row is a center\nThe indices on the left are the labels\n", centers)
-    results = pd.concat([relevant, pd.Series(kmeans.labels_, index=X.index, dtype='Int64', name='kmeans cluster')], axis=1)
-    print("\nRelevant data from spreadsheet with its determined cluster\n", results)
+    centers.index.name = 'Center label'
+    labeled_data = pd.concat([relevant, pd.Series(kmeans.labels_, index=X.index, dtype='Int64', name='kmeans cluster')], axis=1)
+    labeled_data.index.name = "Spreadsheet row"
+    last_used_column = libscol.int2scol(len(spreadsheet.columns))
+    range_name = last_used_column + str(COMPOUND_START_ROW) + ':' + last_used_column + str(COMPOUND_END_ROW)
+    if input(f'Do you want to write to the range: {range_name} and its title range? yes?') == 'yes':
+        range_names = [
+        batch_update_values(SPREADSHEET_ID,
+                      range_names, "USER_ENTERED",
+                      labeled_data['kmeans cluster'])
+
+    
