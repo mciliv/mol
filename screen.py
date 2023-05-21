d31bb6255cf1f2848287293af3ee3fed77b712be
M <refits.minxes0h@icloud.com>
2023-05-21 12:15:33 -0700

    corrected test; ipynb restructure for viz

--- screen.py
+++ screen.py
@@ -22,26 +22,27 @@ def args():
     parser.add_argument('-r', '--redock', type=str)
     parser.add_argument('-c', '--write-compounds', action='store_true')
     parser.add_argument('-a', '--dock-all', action='store_true', help='Docks all combos')
-    parser.add_argument('-o', '--output-dir', default=Path(__file__).resolve().parent / "data")
+    parser.add_argument('-d', '--data-dir', default=Path(__file__).resolve().parent / "data")
     return parser.parse_args()
 
 
 def data_dir():
     return util.data_dir(__file__)
 
-def gnina_command(receptor_pdb, ligand_sdf, output_sdf):
+def gnina_command(receptor_pdb, ligand_sdf, gnina_sdf):
     gnina_path = shutil.which("gnina")
     if not gnina_path:
         gnina_path = "/home/m/cure/gnina"
     return [gnina_path, "-r", receptor_pdb,
                "-l", ligand_sdf,
-               "--autobox_ligand", ligand_sdf,
-               "-o", output_sdf,
-               "--seed", "0"]
+               "--autobox_ligand", receptor_pdb,
+               "-o", gnina_sdf,
+               "--seed", "0",
+               "--exhaustiveness", "64"]
 
 
-def dock(receptor: Path, ligand: Path, output_stem: Path, sec_limit=60):
-    dock = {file_type: output_stem.with_suffix("." + file_type) for file_type in ("sdf", "txt")}
+def dock(receptor: Path, ligand: Path, gnina_stem: Path, sec_limit=240):
+    dock = {file_type: gnina_stem.with_suffix("." + file_type) for file_type in ("sdf", "txt")}
     if not (dock["sdf"].exists() and dock["txt"].exists()):
         try:
             with open(dock["txt"], 'a') as dock_txt_file:
@@ -55,7 +56,7 @@ def dock(receptor: Path, ligand: Path, output_stem: Path, sec_limit=60):
                     if poll is not None:
                         dock_txt_file.write(f"Ran for {duration} sec; process poll value is {poll}")
                         stopped = True
-                        logging.info(f"Finished: {output_stem}")
+                        logging.info(f"Finished: {gnina_stem}")
                         break
                 if not stopped:
                     dock_txt_file.write(f"Terminated with {sec_limit} sec limit")
@@ -64,14 +65,14 @@ def dock(receptor: Path, ligand: Path, output_stem: Path, sec_limit=60):
     return dock
 
 
-def dock_all(receptor_dir='receptors', compound_dir='compounds', output_dir='docks'):
-    data_paths = {dir: data_dir() / dir for dir in [receptor_dir, compound_dir, output_dir]}
+def dock_all(receptor_dir='receptors', compound_dir='compounds', gnina_dir='docks'):
+    data_paths = {dir: data_dir() / dir for dir in [receptor_dir, compound_dir, gnina_dir]}
     for receptor in sorted(data_paths[receptor_dir].iterdir()):
-        receptors_dock_dir = util.mkdirs(data_paths[output_dir] / receptor.stem)
+        receptors_dock_dir = util.mkdirs(data_paths[gnina_dir] / receptor.stem)
 
         for ligand_sdf in data_paths[compound_dir].iterdir():
-            output_stem_path = receptors_dock_dir / (ligand_sdf.stem + "_" + receptor.stem)
-            dock(receptor, ligand_sdf, output_stem_path)
+            gnina_stem_path = receptors_dock_dir / (ligand_sdf.stem + "_" + receptor.stem)
+            dock(receptor, ligand_sdf, gnina_stem_path)
 
 
 def pdb_partition(pdb_path: Path, identifier: str):
@@ -86,7 +87,7 @@ if __name__ == "__main__":
     if args.write_compounds:
         chem.compounds()
     if args.dock_all:
-        dock_all(rcsb_pdb.apoprotein(['fabp4', 'fabp5']).stem)
+        dock_all(receptor_dir=rcsb_pdb.apoprotein(['fabp4', 'fabp5']).stem, gnina_dir='docks_whole_protein_box')
 
 
 
