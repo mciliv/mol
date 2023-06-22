cd22338ce6b99619a75f20f05492317a21753f4d
M <refits.minxes0h@icloud.com>
2023-06-21 20:08:59 -0700

    Restructuring, more specific query, correction of directory deletion
    Specified homo sapiens as organism for rcsb protein search; don't clean
    directory when writing files for apoproteins

--- screen.py
+++ screen.py
@@ -7,33 +7,49 @@ import argparse
 import logging
 from pathlib import Path
 import time
-from datetime import datetime
+
+from Bio.PDB import PDBList
 
 import filing
+import processing
 import log
 import chem
 import rcsb_pdb
 
 
+def main():
+    log.setup(__file__)
+    args = parsed_args()
+    if args.write_compounds:
+        chem.compounds()
+    compounds = args.compounds
+    if args.default_compounds:
+        compounds |= {compound_path.stem for compound_path in chem.compound_dir().iterdir()}
+    compounds -= args.compound_excludes
+    receptor_dir = local_data_dir() / "receptors"
+    filing.mkdirs(receptor_dir, clean=True)
+    for pdb_id in args.receptors_pdb_ids:
+        rcsb_pdb.write_pdb(pdb_id, receptor_dir)
+    rcsb_pdb.apoproteins(args.receptors, receptor_dir)
+    if args.redos is not None:
+        compounds |= {Path(path).stem.split('_')[0] for path in args.redos}
+    dock_batch(
+        compounds=compounds, receptor_dir=receptor_dir, gnina_dir=args.dock_dir, default_compounds=args.default_compounds, sec_limit=args.sec_limit
+    )
+
+
 def parsed_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--compounds", default=set(), nargs="*", type=str)
-    parser.add_argument("-w", "--write-compounds", action="store_true")
+    parser.add_argument("-w", "--write-compounds", help="Overwrites existing compounds", action="store_true")
     parser.add_argument("--default-compounds",  action='store_true')
     parser.add_argument("-e", "--compound-excludes", help="Excludes from all", default={"Insulin"}, nargs="*", type=str)
-    parser.add_argument("-r", "--receptors", default={"fabp4", "fabp5"}, nargs="*", type=str)
-    parser.add_argument(
-        "-o",
-        "--dock-dir",
-        default=f"docks_ligand_autoboxed_{datetime.now().isoformat()}",
-    )
-    parser.add_argument(
-        "-d", "--data-dir", default=local_data_dir()
-    )
-    parser.add_argument(
-        "-a", "--redos", nargs=argparse.REMAINDER, type=str
-    )
+    parser.add_argument("-i", "--receptors-pdb-ids", default = {"3rzy", "4lkp"}, nargs="*", type=str)
+    parser.add_argument("-r", "--receptors", default=set(), help="Searches pdb database", nargs="*", type=str)
+    parser.add_argument("-o", "--dock-dir", default="docks")
+    parser.add_argument("-d", "--data-dir-path", default=local_data_dir())
     parser.add_argument("-s", "--sec-limit", default=float('inf'), type=float)
+    parser.add_argument("-a", "--redos", nargs=argparse.REMAINDER, type=str)
     return parser.parse_args()
 
 
@@ -42,16 +58,14 @@ def local_data_dir():
 
 
 def dock_batch(compounds, receptor_dir="receptors", compound_dir="compounds", gnina_dir="docks", default_compounds=False, sec_limit=float("inf")):
-    data_paths = {
-        dir: local_data_dir() / dir for dir in [receptor_dir, compound_dir, gnina_dir]
-        }
+    data_paths = {dir: local_data_dir() / dir for dir in [receptor_dir, compound_dir, gnina_dir]}
     for receptor in sorted(data_paths[receptor_dir].iterdir()):
         receptors_dock_dir: Path = filing.mkdirs(data_paths[gnina_dir] / receptor.stem)
 
         for compound in data_paths[compound_dir].iterdir():
-            if not default_compounds and not filing.matches_ignoring_spaces_and_line_symbols(compound.stem, compounds):
-                continue
-            dock(receptor, compound, receptors_dock_dir, sec_limit)
+            if filing.matches_ignoring_spaces_and_line_symbols(compound.stem, compounds):
+                dock(receptor, compound, receptors_dock_dir, sec_limit)
+    return data_paths[gnina_dir]
 
 
 def dock(receptor: Path, ligand: Path, destination_dir: Path=Path.cwd(), sec_limit=float('inf')):
@@ -71,21 +85,7 @@ def dock(receptor: Path, ligand: Path, destination_dir: Path=Path.cwd(), sec_lim
                         stdout=dock_txt_file,
                         stderr=dock_txt_file,
                         )
-                stopped = False
-                sec = 0
-                while sec < sec_limit:
-                    time.sleep(1)
-                    poll = docking.poll()
-                    if poll is not None:
-                        dock_txt_file.write(
-                                f"Ran for {sec} sec; process poll value is {poll}"
-                                )
-                        stopped = True
-                        logging.info(f"Finished: {dock_result['sdf'].stem}")
-                        break
-                    sec += 1
-                if not stopped:
-                    dock_txt_file.write(f"Terminated with {sec_limit} sec limit")
+                processing.limit(docking, sec_limit, dock_txt_file)
             chem.transfer_smiles_attribute(dock_result["sdf"])
         except subprocess.CalledProcessError as e:
             logging.error(f"Error processing {receptor} and {ligand}: {e}")
@@ -117,9 +117,9 @@ def gnina_command(receptor, ligand, gnina_result):
 
 
 def autobox_ligand(receptor_name: str) -> dict:
-    receptors_of_pdbs = {"3RZY": "fabp4", "4LKP": "fabp5"}
-    ref_complexes = {"fabp4": "2NNQ", "fabp5": "5HZ5"}
-    ref_ligands = {"2NNQ": "T4B", "5HZ5": "65X"}
+    receptors_of_pdbs = {"3rzy": "fabp4", "4lkp": "fabp5"}
+    ref_complexes = {"fabp4": "2nnq", "fabp5": "5hz5"}
+    ref_ligands = {"2nnq": "T4B", "5hz5": "65X"}
     ref_complex = ref_complexes[receptors_of_pdbs[receptor_name]]
     ref_ligand = ref_ligands[ref_complex]
     complexes: Path = local_data_dir() / "complexes"
@@ -130,17 +130,4 @@ def autobox_ligand(receptor_name: str) -> dict:
 
 
 if __name__ == "__main__":
-    log.setup(__file__)
-    args = parsed_args()
-    if args.write_compounds:
-        chem.compounds()
-    compounds = args.compounds
-    if args.default_compounds:
-        compounds |= {compound_path.stem for compound_path in Path(args.data_dir).iterdir()}
-    compounds -= args.compound_excludes
-    if args.redos is not None:
-        compounds |= {Path(path).stem.split('_')[0] for path in args.redos}
-    dock_batch(
-        compounds=compounds, receptor_dir=rcsb_pdb.apoproteins(args.receptors), gnina_dir=args.dock_dir, default_compounds=args.default_compounds, sec_limit=args.sec_limit
-    )
-
+    main()
