cf633d5ed671a169ab88e9d95619a27a42ff9c23
M <6300735+mciliv@users.noreply.github.com>
2025-03-05 23:19:28 -0800

    seperation of file types & auto func call for sdf

--- src/usdz.py
+++ src/usdz.py
@@ -1,36 +1,3 @@
-from rdkit import Chem
-from rdkit.Chem import SDWriter
-
-def sdf(smiles, overwrite=False, directory_path=compound_dir()):
-    destination = directory_path / Path(smiles).with_suffix(".sdf")
-    if overwrite or not destination.exists():
-        try:
-            mol = Chem.MolFromSmiles(smiles)
-            Chem.Kekulize(mol)
-            mol = Chem.AddHs(mol)
-            AllChem.EmbedMolecule(mol)
-            Chem.RemoveHs(mol)
-            
-            mol.SetProp("SMILES", smiles) 
-            
-            with open(destination, "w") as file:
-                with SDWriter(file) as writer:
-                    writer.write(mol)
-        except Exception as e:
-            logging.exception(f"\n{name}\n{e}\n")
-    return destination
-
-
-def is_valid_sdf_with_molecule(filepath):
-    try:
-        supplier = Chem.SDMolSupplier(str(filepath))
-        for mol in supplier:
-            if mol:
-                return True
-    finally:
-        return False
-
-
 import os
 from openbabel import openbabel
 from pxr import Usd, UsdGeom, UsdShade, Sdf
