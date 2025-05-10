831844a520513986c57c6eba24dcaf4ac499c4bf
M <6300735+mciliv@users.noreply.github.com>
2025-05-09 23:58:27 -0700

    helper.sh sets up proj

---
+++ helper.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+PYVERSION=3.12.9
+
+if ! command -v pyenv &> /dev/null; then
+    curl https://pyenv.run | bash
+    export PATH="$HOME/.pyenv/bin:$PATH"
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+fi
+pyenv install -s $PYVERSION
+pyenv local $PYVERSION
+
+curl -sSL https://install.python-poetry.org | python3 -
+export PATH="$HOME/.local/bin:$PATH"
+poetry env use $PYVERSION
+eval $(poetry env activate)
+export PYTHONPATH=$(pwd)
+poetry sync --no-root
+echo "To deactivate env, run: exit"
